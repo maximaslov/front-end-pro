7afe687b187d9adc26d32e64f9c8065d7949056b
Maxim <masmaxvad@gmil.com>
2022-08-19 14:11:39 +0300

    done

---
+++ 23-homework/nodejs/index.js
@@ -0,0 +1,6 @@
+const calculator = require('../nodejs/src/calculator.js');
+
+console.log(calculator.add(10, 2));
+console.log(calculator.sub(12, 5));
+console.log(calculator.mult(7, 4));
+console.log(calculator.div(28, 2));
